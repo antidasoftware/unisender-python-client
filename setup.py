faedcf7f80074202d395e80920968c9feb38e553
azat.zakirov <zakirmalay@gmail.com>
2020-02-11 09:56:27 +0500

    update setup.y

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-unisender",
+    name="unisender-python-client",
     version="0.0.1",
     author="Azat Zakirov",
     author_email="zakirmalay@gmail.com",
